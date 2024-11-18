563254f0a0a5d6030274cdfd9bc56f0699852d27
Yehor <yehor12344321@gmail.com>
2024-11-18 20:22:01 +0200

    зберігає зміни

--- src/partials/js/api-server.js
+++ src/partials/js/api-server.js
@@ -1,3 +1,8 @@
 const BASE_URL = 'app.ticketmaster.com'
 const API_KEY = '2V21koh33DKniuaGAcV6NpSm4RteCUeI'
 
+export function getDetailsOfEvent(){
+    return fetch(`https://${BASE_URL}/discovery/v2/events/G5dIZb8kECzK3?apikey=${API_KEY}`)
+    .then(res=> res.json())
+}
+
---
+++ src/partials/js/modal.js
@@ -0,0 +1,38 @@
+import {getDetailsOfEvent} from './api-server'
+
+const modalBackdrop = document.querySelector('.backdrop')
+const modal = document.querySelector('.modal')
+const li = document.querySelector('li')
+
+li.addEventListener('click', onListItemClick)
+
+function createModalMarkup(accident){
+    return `<h3 class="title-info">INFO</h3>
+    <p >${accident.promoter.description}</p>         
+    <h3 class="title-when">WHEN</h3>         
+    <p>${accident.dates.start.localDate}</p>         
+    <p>${accident.dates.start.localTime} (${accident.dates.timezone})</p>         
+    <h3 class="title-where">WHERE</h3>         
+    <p>${accident._embedded.venues[0].city.name}, ${accident._embedded.venues[0].country.name}</p>         
+    <p>${accident._embedded.venues[0].name}</p>         
+    <h3 class="title-who">WHO</h3>         
+    <p>${accident.name}</p>         
+    <h3 class="title-price">PRICES</h3>         
+    <p>${accident.priceRanges[0].type} ${accident.priceRanges[0].min}-${accident.priceRanges[0].max} ${accident.priceRanges[0].currency}</p>         
+    <button type="button">BUY TICKETS</button>           
+    <img src="${accident.images[9].url}" alt="">         
+    <img src="${accident.images[7].url}" alt="">` }
+
+function onListItemClick(){
+    getDetailsOfEvent()
+    .then(accident =>{
+        const modalMarkUp = createModalMarkup(accident)
+        modal.innerHTML = modalMarkUp
+    })
+    
+}
+
+
+
+
+
