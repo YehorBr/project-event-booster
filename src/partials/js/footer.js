270e2165ed05b7d91142da9269e5ff297ac6dcb7
butilkf228 <129097758+butilkf228@users.noreply.github.com>
2024-12-02 18:52:04 +0100

    s

--- src/partials/js/footer.js
+++ src/partials/js/footer.js
@@ -1,40 +1,66 @@
-const options = {
-    totalItems: 10,
-    itemsPerPage: 10,
-    visiblePages: 10,
-    page: 1,
-    centerAlign: false,
-    firstItemClassName: 'tui-first-child',
-    lastItemClassName: 'tui-last-child',
-    template: {
-      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-      currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-      moveButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}">' +
-          '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</a>',
-      disabledMoveButton:
-        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-          '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</span>',
-      moreButton:
-        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-          '<span class="tui-ico-ellip">...</span>' +
-        '</a>'
-    }
-  };
+import Pagination from 'tui-pagination';  
+
+// const Pagination = tui.Pagination;
+
+// const container = document.getElementById('pagination');
+// const options = { // below default value of options
+//      totalItems: 10,
+//      itemsPerPage: 10,
+//      visiblePages: 10,
+//      page: 1,
+//      centerAlign: false,
+//      firstItemClassName: 'tui-first-child',
+//      lastItemClassName: 'tui-last-child',
+//      template: {
+//          page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+//          currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+//          moveButton:
+//              '<a href="#" class="tui-page-btn tui-{{type}}">' +
+//                  '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//              '</a>',
+//          disabledMoveButton:
+//              '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+//                  '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//              '</span>',
+//          moreButton:
+//              '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+//                  '<span class="tui-ico-ellip">...</span>' +
+//              '</a>'
+//      }
+// };
+// const pagination = new Pagination(container, options);
+
+
+
+  // Usage!!!
+
+  // const Pagination = tui.Pagination;
+
+  // const container = document.getElementById('tui-pagination-container');
+  // const instance = new Pagination(container, { ... });
   
-  const pagination = new Pagination('pagination', options);
+  // instance.getCurrentPage();
 
-moveButton: type => {
-    let template = '';
+  // Usage!!!
 
-    if (type === 'first') {
-      template =
-        '<div class="custom-page-btn">' +
-          '<span class="custom-ico"></span>' +
-        '</div>';
-    }
 
-    return template;
-}
+  // Using custom events!!!
+
+  var pagination = new tuiPagination('pagination', {
+    totalItems: 500
+});
+
+pagination.on('beforeMove', function(eventData) {
+    console.log("before move")
+});
+
+pagination.on('afterMove', function(eventData) {
+    console.log("after move")
+});
+
+
+// button.addeventlistener("click", () => {
+//     // запит на бек енд
+// });
+
+  // Using custom events!!!
