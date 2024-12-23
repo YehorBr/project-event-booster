ce88bc695b585e39a0d434e9a48cb5798d917be4
Denis Zavhorodnii <zdenik45@gmail.com>
2024-12-23 19:13:00 +0200

    123

--- src/partials/js/api-server.js
+++ src/partials/js/api-server.js
@@ -4,6 +4,12 @@ const API_KEY = '2V21koh33DKniuaGAcV6NpSm4RteCUeI'
 
 export function searchByCountry(country) {
     console.log(country);
-    return fetch(`${BASE_URL}/discovery/v2/events?locale=${country}&apikey=${API_KEY}`)
+    return fetch(`https://${BASE_URL}/discovery/v2/events?locale=${country}&apikey=${API_KEY}`)
+    .then(r=>r.json())
+}
+
+export function searchByKeyword(keyword) {
+    console.log(keyword);
+    return fetch(`https://${BASE_URL}/discovery/v2/events?locale=${keyword}&apikey=${API_KEY}`)
     .then(r=>r.json())
 }
--- src/partials/js/header.js
+++ src/partials/js/header.js
@@ -1,14 +1,13 @@
 import { searchByCountry } from "./api-server";
+import { searchByKeyword } from "./api-server";
 
 const select = document.getElementById('country_select')
 
 select.addEventListener('change', (event) =>{
     console.log(event.currentTarget.value);
     searchByCountry(event.target.value)
-    .then(events => {console.log(events);})
+    .then(events => {
+        const eventsByCountry = events._embedded.events
+        // викликати функціїю яка рендерить події і передати їй eventsByCountry
+    })
 })
-
-function searchByKeyword(keyword) {
-    return fetch('https:....${keyword}')
-    .then(r=>r.json())
-}
