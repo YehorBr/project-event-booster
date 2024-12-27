35446ae00cae3224cbcd11f202f8f91caa0641f3
TimMedin <timmedin180@gmail.com>
2024-12-27 20:27:25 +0200

    loader

--- src/partials/js/main.js
+++ src/partials/js/main.js
@@ -9,16 +9,20 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 async function fetchAndRenderEvents() {
-  try {
-    const events = await ticketmasterApi.fetchEvents();
-    if (events.length === 0) {
-      return;
-    }
+  const loader = document.createElement('div');
+  loader.className = 'loader';
+  articlesContainerRef.appendChild(loader);
 
-    const firstThreeEvents = events.slice(0, 20);
-    createMarkup(firstThreeEvents);
+  try {
+    loader.style.display = 'block';
+    const events = await ticketmasterApi.fetchEvents();
+    if (events.length === 0) return;
+    const firstTwentyEvents = events.slice(0, 20);
+    createMarkup(firstTwentyEvents);
   } catch (error) {
     console.error('Error', error);
+  } finally {
+    loader.remove();
   }
 }
 
@@ -35,6 +39,5 @@ export function createMarkup(events) {
     `
     )
     .join('');
-
   articlesContainerRef.insertAdjacentHTML('beforeend', markup);
 }
