81f4a9933794846b81c579d514e79265cb70eff3
Yehor <129083741+YehorBr@users.noreply.github.com>
2026-01-03 06:10:53 +0100

    saves changes

--- src/partials/js/modal.js
+++ src/partials/js/modal.js
@@ -14,27 +14,39 @@ modalBackdrop.addEventListener('click', onBackdropClick)
 document.addEventListener('keydown', onEscapeModal)
 
 
-function createModalMarkup(accident){
-    return `<img class="small-img" src="${accident.images[7].url}" alt="">
+function createModalMarkup(accident) {
+  return `
+    <img class="small-img" src="${accident.images?.[7]?.url || accident.images?.[0]?.url || ''}" alt="">
     <div class="content-container">
-    <img class="big-img" src="${accident.images[4].url}" alt="">
+    <img class="big-img" src="${accident.images?.[4]?.url || accident.images?.[0]?.url || ''}" alt="">
+    
     <div class="info-container">  
-    <h3 class="title-info">INFO</h3>
-    <p >${accident.promoter.description}</p>         
-    <h3 class="title-when">WHEN</h3>         
-    <p>${accident.dates.start.localDate}</p>         
-    <p>${accident.dates.start.localTime} (${accident.dates.timezone})</p>         
-    <h3 class="title-where">WHERE</h3>         
-    <p>${accident._embedded.venues[0].city.name}, ${accident._embedded.venues[0].country.name}</p>         
-    <p>${accident._embedded.venues[0].name}</p>         
-    <h3 class="title-who">WHO</h3>         
-    <p>${accident.name}</p>         
-    <h3 class="title-price">PRICES</h3>         
-    <p>${accident.priceRanges[0].type} ${accident.priceRanges[0].min}-${accident.priceRanges[0].max} ${accident.priceRanges[0].currency}</p>         
-    <button type="button">BUY TICKETS</button>
-    </div></div>            
-           
-    ` }
+      <h3 class="title-info">INFO</h3>
+      <p>${accident.promoter?.description || "No description available"}</p>
+
+      <h3 class="title-when">WHEN</h3>         
+      <p>${accident.dates?.start?.localDate || "No date"}</p>         
+      <p>${accident.dates?.start?.localTime || ""} (${accident.dates?.timezone || ""})</p>         
+
+      <h3 class="title-where">WHERE</h3>         
+      <p>${accident._embedded?.venues?.[0]?.city?.name || ""}, 
+         ${accident._embedded?.venues?.[0]?.country?.name || ""}</p>         
+      <p>${accident._embedded?.venues?.[0]?.name || ""}</p>         
+
+      <h3 class="title-who">WHO</h3>         
+      <p>${accident.name || ""}</p>         
+
+      <h3 class="title-price">PRICES</h3>         
+      <p>
+        ${accident.priceRanges?.[0]?.type || ""} 
+        ${accident.priceRanges?.[0]?.min || ""} -
+        ${accident.priceRanges?.[0]?.max || ""} 
+        ${accident.priceRanges?.[0]?.currency || ""}
+      </p>         
+
+      <button type="button">BUY TICKETS</button>
+    </div></div>`;
+}
 
 function onArticlesClick(e){
     if(e.target.parentNode.nodeName !== 'LI'){
